4b108b46c25f31b22b1e66ef7792948eeb66d245
GokhanCingoz <gokhancingoz1@gmail.com>
2023-03-12 11:38:01 +0300

    Actions changed to async

--- Bloggie.Web/Controllers/AdminTagsController.cs
+++ Bloggie.Web/Controllers/AdminTagsController.cs
@@ -2,109 +2,125 @@
 using Bloggie.Web.Models.Domain;
 using Bloggie.Web.Models.ViewModels;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.IdentityModel.Tokens;
+using Microsoft.EntityFrameworkCore;
 
 namespace Bloggie.Web.Controllers
 {
     public class AdminTagsController : Controller
     {
+        private readonly BloggieDbContext bloggieDbContext;
 
-        private BloggieDbContext _bloggieDbContext;
+        //Dependency Injection --> Daha öncesinde DbContext'e özgü bir class tanımladık ve program.cs içerisinde bu classı service özelliklerinden uyglamamıza tanıttık. Bunu yqpmaktaki amacımız ihtiyaç duyulan herhangi bir nesne içerisinde ihtiyaç duyduğumuz bu nesneyi çağırabilmek. Ve nesnenin içerisinde yeniden bu dbcontexi oluşturmadan, mevcutta olan dbcontext üzerine erişim sağlamaktı. Yani DbConteximizi istediğimiz her classa enjekte edebiliyoruz. Bu işleme dependency injection denmektedir. Bu örnekte bunu kullanabilmek için mevcut classımızda bir constructor oluşturduk ve parametre olarak Dbcontext nesnemizden bir argüman yolladık. Bu argüman classın tamamında kullanılamayacağı için bir private field oluşturduk ve bu field ile constructordan gelen argümanı eşitleyerek classımız içerisinde field'ımız üzerinden dbcontext nesnesini kullanabiliyor hale geldik. 
+
         public AdminTagsController(BloggieDbContext bloggieDbContext)
         {
-            _bloggieDbContext = bloggieDbContext;
+            this.bloggieDbContext = bloggieDbContext;
         }
 
-        // Dependency Enjection ==> Daha öncesinde DbContex'e özgü bir class tanımladık ve program.cs içerisinde bu classı service özelliklerinden uygulamamıza tanıttık. Bunu yapmaktaki amacımız ihtiyaç duyulan herhangi bir nesne içerisinde ihtiyaç duyduğumuz bu nesneyi çağırabilmek. Ve nesnenin içerisinde yeniden bu dbContex'i oluşturmadan, mevcutta onal dBcontext üzerine erişim sağlamaktı. Yani Dbconteximizi istediğimiz her classa enjekte edebiliyoruz.bu işleme dependency indejtion denmektedir. Bu örnekte bunu kullanabilmek için mevcut classımızda bir constructor oluşturduk ve parametre olarak dbContext nesnemizden bir argüman yolladık. Bu argüman classın tamamında kullanılamayacağı için bir private field oluştuduk ve bu field ile contsructurdan gelen argümanı eşitleyerek classımız içerisinde fieldımız üzerinden dbContex nesenesini kullanabiliyor hale geldik.
-
-
-        // Tag ekleme get metodu
+        //Tag ekleme get metodu
         [HttpGet]
-        public IActionResult Add()
+        public async Task <IActionResult> Add()
         {
             return View();
         }
 
-        // Tag ekleme post metodu
+        //tag ekleme post metodu
         [HttpPost]
         [ActionName("Add")]
-        public IActionResult Add(AddTagRequest addTagRequest)
+        public async Task<IActionResult> Add(AddTagRequest addTagRequest)
         {
             //var name= Request.Form["name"];
             //var displayName = Request.Form["displayName"];
 
             //var name = addTagRequest.Name;
             //var display = addTagRequest.DisplayName;
-
             var tag = new Tag
             {
-
                 Name = addTagRequest.Name,
-                DisplayName = addTagRequest.DisplayName,
-
+                DisplayName = addTagRequest.DisplayName
             };
 
-            _bloggieDbContext.Tags.Add(tag);
-            _bloggieDbContext.SaveChanges();
+            await bloggieDbContext.AddAsync(tag);
+            await bloggieDbContext.SaveChangesAsync();
             return RedirectToAction("List");
-
         }
 
-        // Tag listeleme metodu
+
+        //tagleri listeleme metodu
         [HttpGet]
-        public IActionResult List()
+        public async Task<IActionResult> List()
         {
-            var tags = _bloggieDbContext.Tags.ToList();
+            var tags = await bloggieDbContext.Tags.ToListAsync();
             return View(tags);
         }
 
         //Edit Sayfasının görüntülenme(get) actionu
         [HttpGet]
-        public IActionResult Edit(Guid id)
+        public async Task<IActionResult> Edit(Guid id)
         {
-            var tag = _bloggieDbContext.Tags.FirstOrDefault(x => x.Id == id);
+            //1. metot
+            //var tag = bloggieDbContext.Tags.Find(id);
+
+            //2.metot
+            var tag = await bloggieDbContext.Tags.FirstOrDefaultAsync(t => t.Id == id);
 
             if (tag != null)
             {
                 var editTagReq = new EditTagRequest
                 {
-                    Id = id,
+                    Id = tag.Id,
                     Name = tag.Name,
-                    DisplayName = tag.DisplayName,
+                    DisplayName = tag.DisplayName
                 };
                 return View(editTagReq);
             }
-            else
-            {
-                return View(null);
-            }
+
+            return View(null);
 
         }
 
-
         [HttpPost]
-        public IActionResult Edit(EditTagRequest editTagRequest)
+        public async Task<IActionResult> Edit(EditTagRequest editTagRequest)
         {
             var tag = new Tag
             {
                 Id = editTagRequest.Id,
                 Name = editTagRequest.Name,
-                DisplayName = editTagRequest.DisplayName,
+                DisplayName = editTagRequest.DisplayName
             };
 
-            var existingTag = _bloggieDbContext.Tags.FirstOrDefault(y => y.Id == tag.Id);
+            var existingTag = await bloggieDbContext.Tags.FindAsync(tag.Id);
 
-            if (existingTag!=null)
+            if (existingTag != null)
             {
                 existingTag.Name = tag.Name;
                 existingTag.DisplayName = tag.DisplayName;
 
-                _bloggieDbContext.SaveChanges();
+                await bloggieDbContext.SaveChangesAsync();
 
-                //return RedirectToAction("Edit", new {id=editTagRequest.Id});
+                //return RedirectToAction("Edit", new { id = editTagRequest.Id});
                 return RedirectToAction("List");
             }
+
+            return RedirectToAction("Edit", new { id = editTagRequest.Id });
+
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(EditTagRequest editTagRequest)
+        {
+            var tag = await bloggieDbContext.Tags.FindAsync(editTagRequest.Id);
+
+            if (tag != null)
+            {
+                bloggieDbContext.Remove(tag);
+                await bloggieDbContext.SaveChangesAsync();
+
+                return RedirectToAction("List");
+            }
+
             return RedirectToAction("Edit", new { id = editTagRequest.Id });
         }
+
     }
 }
